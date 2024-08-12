bc7c747f7faf8f61b9ef2e29ded2d6b793a6f302
Gold Exile <159054302+Arcanjoo@users.noreply.github.com>
2024-08-12 13:53:06 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE Funcionarios(
+  Nome varchar(50),
+  CPF varchar(11),
+  Salario varchar(5),
+  Setor varchar(8),
+  Contratação varchar(8)
+  );
+CREATE TABLE produtos(
+descricao varchar(50),
+  preco varchar(20),
+  quatidade varchar(10),
+  total varchar(5)
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
